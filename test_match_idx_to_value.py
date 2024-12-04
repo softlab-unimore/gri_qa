8a96313b44c4b4484c4a28ffcd9fc98886f01225
Luca Contalbo <luca.contalbo@gmail.com>
2024-12-04 12:18:22 +0100

    fix(dataset): integrated matteo's annotations

--- test_match_idx_to_value.py
+++ test_match_idx_to_value.py
@@ -10,7 +10,7 @@ def remove_perc(num_str):
     num_str = num_str.rstrip('%')
     return num_str
 
-dataset = pd.read_csv("../dataset/qa_dataset.csv", sep=",")
+dataset = pd.read_csv("./dataset/qa_dataset.csv", sep=",")
 dataset["Error (0 no error, 1 value err, 2 unrelated, 3 hierarchical)"] = dataset["Error (0 no error, 1 value err, 2 unrelated, 3 hierarchical)"].fillna(0.0)
 
 dataset = dataset[dataset["Error (0 no error, 1 value err, 2 unrelated, 3 hierarchical)"].isin([0.0,1.0,3.0])]
@@ -18,8 +18,8 @@ dataset = dataset[dataset["row"].notna()]
 
 total_errors = 0
 for i,row in dataset.iterrows():
-    path = f"../dataset/annotation/{'.'.join(row['pdf name'].split('.')[:-1])}/{row['page nbr']}_{row['table nbr']}.csv"
-    table = pd.read_csv(path, sep=";")
+    path = f"./dataset/annotation/{'.'.join(row['pdf name'].split('.')[:-1])}/{row['page nbr']}_{row['table nbr']}.csv"
+    table = pd.read_csv(path, sep=";", encoding="latin1")
     gold = remove_trailing_zeros(remove_perc(str(row['value']).strip()))
     try:
         from_idx = remove_trailing_zeros(remove_perc(str(table.iloc[int(row['row'])-2, int(row['column'])-1]).strip()))
