0ae1734d028da0956ddc667296e32ebdcdb7d540
sarapeddy <269362@studenti.unimore.it>
2024-12-18 10:13:53 +0100

    Add the filtering on the third column with value 2

--- test_openapi.py
+++ test_openapi.py
@@ -109,6 +109,7 @@ def table_predictions(qa_file: str, dataset_dir: str, save_dir: str) -> pd.DataF
     """
     # Load initial QA data and prepare index
     df_qa = pd.read_csv(qa_file)
+    df_qa = df_qa[df_qa.iloc[:, 2] != 2.0]
     df_qa = df_qa[['question', 'pdf name', 'page nbr', 'table nbr', 'value']].astype(str)
     df_qa['index'] = np.arange(len(df_qa))
 
