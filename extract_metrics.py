847297ec51e41304557bcbd5452df205b106046b
sarapeddy <269362@studenti.unimore.it>
2024-12-18 11:02:18 +0100

    fix evalutation and filtering on openai

--- extract_metrics.py
+++ extract_metrics.py
@@ -34,8 +34,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # models = ['tatllm', 'tapex', 'tablellama', 'finma', 'tagop', 'openai']
-    models = ['tatllm__end_to_end', 'tapex', 'tablellama', 'finma', 'openai']
-    # models = ['finma', 'tapex', 'tablellama', 'openai']
+    # models = ['tatllm__end_to_end', 'tapex', 'tablellama', 'finma', 'openai']
+    models = ['finma', 'tapex', 'tablellama', 'openai', 'tatllm__end_to_end']
 
     metrics = pd.DataFrame(columns=['model', 'em'])
 
