99ee7405891d47e2dfb39efa7e864b30018b72f0
Luca Contalbo <37805862+lucacontalbo@users.noreply.github.com>
2025-02-12 12:14:49 +0100

    Update prompts.py

--- table_extraction/prompts.py
+++ table_extraction/prompts.py
@@ -1,14 +1,14 @@
-system_prompt = """Ti verrà fornita una tabella HTML strutturalmente corretta ma con alcuni valori numeri sbagliati.
-Inoltre, te ne verrà fornita una seconda con i valori numerici corretti.
-Correggi la prima tabella inserendo i valori corretti e ritornala in formato csv.
-Scrivi esclusivamente il file csv. Non scrivere nient'altro.
+system_prompt = """You will be provided with an HTML table that is structurally correct but contains some incorrect numerical values.
+Additionally, you will be given a second table with the correct numerical values.
+Correct the first table by inserting the correct values and return it in CSV format.
+Write only the CSV file. Do not write anything else.
 """
 
-human_prompt = """# TABELLA 1
+human_prompt = """# TABLE 1
 
 {}
 
-# TABELLA 2
+# TABLE 2
 
 {}
 """
